d08aa9f95f5ac700170e393175e39d1e8908784e
Marc_Franke <hallo@marcfranke.de>
2025-09-03 15:53:48 +0200

    Reverse restructure

--- src/student/ADT/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Stack import Stack
-from .Queue import Queue
-from .DynArray import DynArray
-from .BinTree import BinTree
-
-# Make all classes available when using "from ADT import *"
-__all__ = ['Stack', 'Queue', 'DynArray', 'BinTree']
